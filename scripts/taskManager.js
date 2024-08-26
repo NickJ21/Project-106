90f8b2957eec436c64044f2f7d85daf463fe8ae3
NickJ21 <Nicholas.Johnson@sdgku.edu>
2024-08-26 15:51:29 -0700

    third assignment

---
+++ scripts/taskManager.js
@@ -0,0 +1,55 @@
+function saveTask() {
+    const title = $("#txtTitle").val();
+    const description = $("#txtDescription").val();
+    const color = $("#txtColor").val();
+    const date = $("#startDate").val();
+    const status = $("#selStatus").val();
+    const budget = $("#numBudget").val();
+    console.log(title, description, color, date, status, budget);
+
+    let taskSave = new Task(title, description, color, date, status, budget);
+    console.log(taskSave);
+
+    //save to server (post)
+    $.ajax({
+        type: "post",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+        data: JSON.stringify(taskSave),
+        contentType: "application/json",
+        success: function (response) {
+            console.log(response)
+        },
+        error: function (error) {
+            console.log(error)
+        }
+    });
+    displayTask(taskSave);
+}
+
+//display from server (get)
+function displayTask(task) {
+    let syntax = `<div class = "task" style="border-color:${task.color}">
+    <div class = "info">
+    <h5> ${task.title} </h5>
+    <p> ${task.description} </p>
+    </div>
+    <label class = "status"> ${task.status}</label>
+    <div class = "date-budget">
+    <label>${task.date} </label> <br>
+    <label>${task.budget} </label> 
+
+    </div>
+    </div>
+    `;
+
+
+
+    $("#list").append(syntax);
+}
+
+function init() {
+    $("#btnSave").click(saveTask);
+}
+
+
+window.onload = init
